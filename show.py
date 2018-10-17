b95e5d9bcfaa08abffb18b2a29cf9bb73a02e198
andrewlloyd04 <lloydas@umich.edu>
2018-10-16 21:35:03 -0400

    added label

--- show.py
+++ show.py
@@ -5,6 +5,7 @@ from button import button
 from treeview import treeview
 from menuBar import menuBar
 from menuFile import menuFile
+from label import label
 import json
 
 
@@ -20,7 +21,8 @@ tree.addFile("a1.h5")
 frame = frame(content.getObject(), values_json["frame"][0], values_json["frame"][1])
 frame1 = frame.getObject()
 button = button(frame1, frame1.quit, values_json["button"][0], values_json["button"][1])
-menubar = menuBar(frame.getObject(), values_json["menubar"][0])
+label = label(content.getObject(), values_json["label"][0], values_json["label"][1])
+# menubar = menuBar(frame.getObject(), values_json["menubar"][0])
 
 
 root.mainloop()

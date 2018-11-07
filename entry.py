bfc0be8b80ddd7aa29170fdc0615224cefc1d235
Lévy Li <levy@MacBook-Pro-de-Levy.local>
2018-11-07 12:00:29 -0500

    modifications on canvas and entry, but may need to rewrite in the future

--- entry.py
+++ entry.py
@@ -2,16 +2,17 @@ from tkinter import *
 from tkinter import ttk
 
 class entry():
-    def __init__(self, parent=None, values={}, position={}):
+    def __init__(self, parent=None, values={}, canvas=None):
         self.text = StringVar()
         self.object = ttk.Entry(parent, textvariable=self.text)
         self.state = dict()
         self.state["visible"] = True
         self.state["rendered"] = False
         self.values = values
-        self.position = position
+        # self.position = position
         self.createWidgets()
-        self.render()
+        # self.render()
+        self.canvas = canvas
 
 
     def createWidgets(self):
@@ -21,9 +22,13 @@ class entry():
         	self.object.insert(0, self.values["textvariable"])
 
 
-
     def getTextVal(self):
-    	print(self.object.get())
+        print('entry value for canvas', self.object.get())
+        v_canvas = self.canvas.get_values()
+        v_canvas["n_bins"] = int(self.object.get())
+        self.canvas.set_values(v_canvas)
+        return self.object.get()
+        # set the canvas from the entry value
 
 
     def getObject(self):
@@ -38,23 +43,23 @@ class entry():
         return self.state
 
 
-    def render(self):
+    def render(self, position):
         self.state["rendered"] = True
         self.state["visible"] = True
-        if "column" in self.position:
-            self.object.grid(column=self.position["column"])
-        if "row" in self.position:
-            self.object.grid(row=self.position["row"])
-        if "sticky" in self.position:
-            self.object.grid(sticky=self.position["sticky"])
-        if "padx" in self.position:
-            self.object.grid(padx=self.position["padx"])
-        if "pady" in self.position:
-            self.object.grid(pady=self.position["pady"])
-        if "columnspan" in self.position:
-            self.object.grid(columnspan=self.position["columnspan"])
-        if "rowspan" in self.position:
-            self.object.grid(rowspan=self.position["rowspan"])
+        if "column" in position:
+            self.object.grid(column=position["column"])
+        if "row" in position:
+            self.object.grid(row=position["row"])
+        if "sticky" in position:
+            self.object.grid(sticky=position["sticky"])
+        if "padx" in position:
+            self.object.grid(padx=position["padx"])
+        if "pady" in position:
+            self.object.grid(pady=position["pady"])
+        if "columnspan" in position:
+            self.object.grid(columnspan=position["columnspan"])
+        if "rowspan" in position:
+            self.object.grid(rowspan=position["rowspan"])
         self.object.grid()
 
 

41f05ee3a309170459d7d4a64033ba2270f0ad39
andrewlloyd04 <lloydas@umich.edu>
2018-11-14 11:41:31 -0500

    updates for calling pika c++ from python

--- pika/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "SimpleAmqpClient/SimpleAmqpClient.h"
-#include <iostream>
-
-using namespace std;
-
-int main(){
-	AmqpClient::Channel::ptr_t connection = AmqpClient::Channel::Create("localhost");
-	
-}
